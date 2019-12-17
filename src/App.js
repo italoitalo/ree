e28b48d3ff14f25708a51c28a510d3916356693d
CABRALBRUNO000 <cabralbruno000@gmail.com>
2019-12-17 16:49:13 -0200

    conflito corrigido

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from 'react';
-import styled from 'styled-components'
-
+import styled from 'styled-components';
 import GlobalStyles from './EstiloGlobal';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import FormCadastro from "./components/formCadastro";
 //import NavPerfil from './components/NavPerfil';
 // import Teste from './components/PadraoPerfil';
-=======
->>>>>>> 549c087556dabb7792022cfa2e66e4aa57910baa
-=======
->>>>>>> 549c087556dabb7792022cfa2e66e4aa57910baa
 
 // import FormCadastro from "./components/formCadastro";
 import PadraoPerfil from './components/PadraoPerfil';
@@ -19,18 +12,9 @@ export default function App() {
 
   return (
     <div>
-<<<<<<< HEAD
-<<<<<<< HEAD
       <FormCadastro />
       {/* <NavPerfil/> */}
-      
-
-        {/* <FormCadastro /> */}
-        <PadraoPerfil/>
-
-        {/* <FormCadastro /> */}
-        <PadraoPerfil/>
-
+      {/* <FormCadastro /> */}
       <GlobalStyles />
     </div>
   );

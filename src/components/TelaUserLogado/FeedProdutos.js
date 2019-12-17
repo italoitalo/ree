cb82eff290a583c3cb3267000af5eff5ce696b05
CABRALBRUNO000 <cabralbruno000@gmail.com>
2019-12-17 19:02:59 -0200

    Inicio da criação da tela de susuário logado

---
+++ src/components/TelaUserLogado/FeedProdutos.js
@@ -0,0 +1,13 @@
+// !    ARQUIVO criado para confeccionar a pagina de produtos disponiveis para os usuários logados
+
+
+import React from 'react';
+import styled from 'styled-components'
+
+export default function FeedProdutos() {
+  return (
+    <>
+        
+    </>
+  );
+}

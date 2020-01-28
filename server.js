7d1c9e44defa782156a9d779065dccd736441417
CABRALBRUNO000 <cabralbruno000@gmail.com>
2020-01-28 17:27:30 -0300

    config

--- server.js
+++ server.js
@@ -29,21 +29,6 @@ module.exports = app
 
 
 const port = process.env.PORT || 5000;
-//rotas
-
-// app.post('/cadastro', (req, res) => {
-//     usuario.create({
-//         nome: req.body.nome,
-//         sobrenome: req.body.sobrenome,
-//         senha: req.body.senha,
-//         email: req.body.email,
-//         celular: req.body.celular,
-//     }).then(() => {
-//         res.send("ususário cadastrado com sucesso")
-//     }).catch((erro) => {
-//         res.send("ususário não cadastrado, houve um erro" + erro)
-//     })
-// }) 
 
 /* 
 app.post('/cadastro', (req, res) => {

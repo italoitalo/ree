eb06c5ed71e29f1660e9da17f9cf816d1b8a8132
italo alves <ialvesjesus01@gmail.com>
2020-01-30 18:53:47 -0200

    adicionado o arquivo  teste.js execute e entre  em  http://localhost:3000/clientes

--- teste.js
+++ teste.js
@@ -1,18 +1,45 @@
-var mysql = require('mysql2');
+// exportando  tudo  que é  necessario para a consulta
+const express = require('express');
+const app =  express()
+const bodyParser = require('body-parser')
+const port = 3000 //porta padrao
+const mysql = require('mysql')
 
-var con = mysql.createConnection({
-    host: 'quadroecia.com',
-    user: 'quadro64_italo',
-    password: '123456',
-    database: 'quadro64_ree'
-});
+//configurando o body parser para pegar POSTS 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-con.connect(function(err) {
-  if (err) throw err;
-  //Select all customers and return the result object:
-  con.query("SELECT * FROM user", function (err, result, fields) {
-    if (err) throw err;
-    // res.send(result)
-    console.log(result);
-  });
-});
+
+
+//definindo as rotas
+const router = express.Router();
+router.get('/', (req, res) => res.json({ message: 'Funcionando!' }));
+app.use('/', router)
+
+router.get('/clientes', (req, res) =>{
+    execSQLQuery('SELECT * FROM user', res);
+})
+
+//concecta ao banco de dados
+function execSQLQuery(sqlQry, res){
+    const connection = mysql.createConnection({
+        host: 'quadroecia.com',
+        user: 'quadro64_italo',
+        password: '123456',
+        database: 'quadro64_ree'
+    });
+  
+    connection.query(sqlQry, function(error, results, fields){
+        if(error) 
+          res.json(error);
+        else
+          res.json(results);
+        connection.end();
+        console.log('executou!');
+    });
+  }
+
+//inicia o servidor
+app.listen(port);
+console.log('API funcionando!');
+

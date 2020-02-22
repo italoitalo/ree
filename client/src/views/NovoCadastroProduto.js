9c3bdf62ab55187a7961fd893273105f40128782
rhuan Gon <rhuan.gon@gmail.com>
2020-02-21 21:38:22 -0300

    atualização de produto
    att

--- client/src/views/NovoCadastroProduto.js
+++ client/src/views/NovoCadastroProduto.js
@@ -4,6 +4,13 @@ import styled from  'styled-components'
 import HeaderPrivt from './../componentes/CabecalhoPrivado'
 /* import Porta from './../img/formCadastroImg/porta.jpg' */
 
+
+import Keyboard from './../img/cadprod/keyboard.svg'
+import Headphone from './../img/cadprod/music-headphones.svg'
+import Phone from './../img/cadprod/mobile-phone.svg'
+import Notebook from './../img/cadprod/open-laptop-computer.svg'
+import Plug from './../img/cadprod/socket.svg'
+
 export default function newform() {
     
     
@@ -19,34 +26,51 @@ export default function newform() {
                     
                     <Box>
                         <Title>Oque quer doar?</Title>
-                        
-                        <input type="radio" name="tipo" id="tipo" value="Celular" required/>Celular
-                        <input type="radio" name="tipo" id="tipo" value="Notebook" required/>Notebook
-                        <input type="radio" name="tipo" id="tipo" value="Fones" required/>Fones
-                        <input type="radio" name="tipo" id="tipo" value="Cabos" required/>Cabos
-                        <input type="radio" name="tipo" id="tipo" value="Gadgets" required/>Gadgets
-                   
-                    </Box> 
+                        <div>
+                            <ImgTipo src={Phone}/>
+                            <ImgTipo src={Notebook}/>
+                            <ImgTipo src={Headphone}/>
+                            <ImgTipo src={Plug}/>
+                            <ImgTipo src={Keyboard}/>
+                        </div>
 
+                        <div>
+                            <RadioInput type="radio" name="tipo" id="tipo" value="Celular" required/>
+                            <RadioInput type="radio" name="tipo" id="tipo" value="Notebook" required/>
+                            <RadioInput type="radio" name="tipo" id="tipo" value="Fones" required/>
+                            <RadioInput type="radio" name="tipo" id="tipo" value="Cabos" required/>
+                            <RadioInput type="radio" name="tipo" id="tipo" value="Gadgets" required/>
+                        </div>
+
+                        <ButtonProx>Próximo</ButtonProx>
+
+                    </Box> 
+                    
                     <Box>
                         <Title>Qual o estado do seu item?</Title>
                         
-                        <input type="radio" name="estado" id="estado" value="Péssimo" required/>Péssimo
-                        <input type="radio" name="estado" id="estado" value="Ruim" required/>Ruim
-                        <input type="radio" name="estado" id="estado" value="Bom" required/>Bom
-                        <input type="radio" name="estado" id="estado" value="Ótimo" required/>Ótimo
-                        <input type="radio" name="estado" id="estado" value="Perfeito" required/>Perfeito
+                        <div><RangeSlide type="range" min="1" max="5" name="estado" id="estado" required/></div>
+                        
+                        <ButtonProx>Próximo</ButtonProx>
+                    
                     </Box> 
 
                     <Box>
                         <Title>Mande uma foto do seu item</Title>
-                        <input type="file" name="fotoProduto" id="fotoProduto" required/>
+                        
+                        <div><input type="file" name="fotoProduto" id="fotoProduto" required/></div>
+                        
+                        <ButtonProx>Próximo</ButtonProx>
                     </Box> 
 
                     <Box>
                         <Title>insira sua Descrição!</Title>
-                        <input type="text" name="nome" id="nome" placeholder="Titulo da doação" required/> 
-                        <input type="text" name="caracteristicas" id="caracteristicas" placeholder="caracteristicas" required/> 
+                        <div>
+                            <input type="text" name="nome" id="nome" placeholder="Titulo da doação" required/> 
+                            <input type="text" name="caracteristicas" id="caracteristicas" placeholder="caracteristicas" required/> 
+                        </div> 
+                        
+                        <ButtonProx>Próximo</ButtonProx>
                     </Box> 
 
                     <input type="text" name="id" id="id"  placeholder="id"/>id
@@ -56,6 +80,37 @@ export default function newform() {
         </>
     )
 }
+
+const ButtonProx = styled.button`
+
+    padding:10px 50px;
+    margin-bottom: 50px;
+    background-image: linear-gradient(to bottom right, rgb(68, 226, 139), rgb(68, 233, 90) );
+    color:white;
+    font-size:14pt;
+    font-weight: bold;
+
+`
+const ImgTipo = styled.img`
+    height:100px;
+    margin: 0px 40px;
+`
+
+const RangeSlide = styled.input`
+
+    width:80%;
+
+`
+
+const RadioInput = styled.input`
+
+    height:15px;
+    width:15px;
+    margin:  10px 85px 90px 85px;
+    cursor:pointer;
+
+`
+
 const Title = styled.h1`
     font-size:35pt;
     color: rgba(0,0,0,0.8);
@@ -63,8 +118,8 @@ const Title = styled.h1`
 
 const Box = styled.section`
     background:white;
-    height:350px;
-    width:1000px;
+    height:100%;
+    width:100%;
     margin-top: -50px;
     margin-bottom:400px;
     border-radius: 10px;
